3c6141aaa6c1d454b7c7789894d0033ba94a63b1
2823558582@qq.com <2823558582@qq.com>
2025-09-18 11:28:38 +0000

    Edited page_use_sh_py_tools

--- client_code/page_use_sh_py_tools/__init__.py
+++ client_code/page_use_sh_py_tools/__init__.py
@@ -12,3 +12,7 @@ class page_use_sh_py_tools(page_use_sh_py_toolsTemplate):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def make_91_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
