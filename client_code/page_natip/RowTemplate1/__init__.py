6007b386db20e1c53bd860c607e5869abb4a5067
2823558582@qq.com <2823558582@qq.com>
2025-09-24 07:24:42 +0000

    Edited page_natip.RowTemplate1

--- client_code/page_natip/RowTemplate1/__init__.py
+++ client_code/page_natip/RowTemplate1/__init__.py
@@ -240,9 +240,14 @@ class RowTemplate1(RowTemplate1Template):
             datas_conf = list(map(lambda x : dict(x) ,datas_conf ))
             txt = list_of_dicts_to_csv_string_readable(datas_conf)
 
-            alert(f"已成功生成 {len(all_conf_after_threads)} 对地址。", title="完成")
+            csv_bytes = ("\ufeff" + txt).encode("utf-8")   # 前置 UTF-8 BOM
 
-            anvil.media.download(anvil.BlobMedia("text/csv", txt.encode(), "wg_conf.csv"))
+            # 3. 下载
+            anvil.media.download(
+                anvil.BlobMedia("text/csv; charset=utf-8", csv_bytes, "wg_conf.csv")
+            )
+
+            alert(f"已成功生成 {len(datas_conf)} 条记录。", title="完成")
 
     
     
