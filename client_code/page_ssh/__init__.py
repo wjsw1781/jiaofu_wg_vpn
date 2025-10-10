a4b37e29d57c29dc44774718f37c151c7c4688f3
2823558582@qq.com <2823558582@qq.com>
2025-10-10 12:42:26 +0000

    Edited page_ssh, page_natip and page_natip.RowTemplate1

--- client_code/page_ssh/__init__.py
+++ client_code/page_ssh/__init__.py
@@ -10,5 +10,9 @@ class page_ssh(page_sshTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        self.repeating_panel_1.items = app_tables.wg_conf.search()
+        # 从指定位置传递过来 只限制这个 ip_to 下的业务数据
+        if 'from_params' in properties:
+            self.repeating_panel_1.items=properties['from_params']
+        else:
+            self.repeating_panel_1.items = app_tables.wg_conf.search()
         # Any code you write here will run before the form opens.
