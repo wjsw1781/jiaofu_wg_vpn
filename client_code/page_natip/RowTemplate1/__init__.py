ad7338f9967d3ab111c65a2208b865a05c28a7d0
2823558582@qq.com <2823558582@qq.com>
2025-10-10 12:48:13 +0000

    Edited page_natip.RowTemplate1

--- client_code/page_natip/RowTemplate1/__init__.py
+++ client_code/page_natip/RowTemplate1/__init__.py
@@ -516,7 +516,7 @@ bash {sh_file}
 
         ip_use_to= self.item['ip_use_to']
         from_params = app_tables.wg_conf.search(ip_to=ip_use_to)
-        page = open_form('page_ssh',from_params=ip_to)
+        page = open_form('page_ssh',from_params=from_params)
         alert(page)
         
 
